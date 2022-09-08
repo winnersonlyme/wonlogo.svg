2cad2c6ad13da9288d66ae720b2493de4e7b849c
Wil (₩) Moore III <51953+wilmoore@users.noreply.github.com>
2022-09-08 14:04:42 -0400

    Initial commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# ₩onlogo.svg
+>⪼ Made with 💜 by realpolyglot.dev
+
+[![Unicode Version: 1.1 (1993)](https://img.shields.io/badge/Unicode%20Version-1.1%20(1993)-blue)](https://www.unicode.org/versions/Unicode1.1.0/)
+
+[wonlogo.svg][] is a transparent [SVG][] based **circular logo** with centered [Unicode][] wordmark. It promotes togetherness and inclusively while also celebrating one's individuality.
+
+If you'd like a personalized [wonlogo.svg][] of your own, [watch][] this repo for more info.
+
+![](https://raw.githubusercontent.com/wilmoore/wonlogo.svg/main/index.svg)
+
+---
+[SVG]: https://www.w3.org/Graphics/SVG/
+[Unicode]: https://unicode-table.com/en/20A9/
+[watch]: https://github.com/wilmoore/wonlogo.svg/watchers
+[wonlogo.svg]: https://github.com/wilmoore/wonlogo.svg
---
+++ userpic.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+declare -x USERNAME="$1"
+declare -x USERPIC="$2"
+
+declare -r DSIMPORT_CMD="/usr/bin/dsimport"
+declare -r ID_CMD="/usr/bin/id"
+
+declare -r MAPPINGS='0x0A 0x5C 0x3A 0x2C'
+declare -r ATTRS='dsRecTypeStandard:Users 2 dsAttrTypeStandard:RecordName externalbinary:dsAttrTypeStandard:JPEGPhoto'
+
+if [ ! -f "${USERPIC}" ]; then
+  echo "User image required"
+fi
+
+# Check that the username exists - exit on error
+${ID_CMD} "${USERNAME}" &>/dev/null || ( echo "User does not exist" && exit 1 )
+
+declare -r PICIMPORT="$(mktemp /tmp/${USERNAME}_dsimport.XXXXXX)" || exit 1
+printf "%s %s \n%s:%s" "${MAPPINGS}" "${ATTRS}" "${USERNAME}" "${USERPIC}" >"${PICIMPORT}"
+${DSIMPORT_CMD} "${PICIMPORT}" /Local/Default M &&
+    echo "Successfully imported ${USERPIC} for ${USERNAME}."
+
+rm "${PICIMPORT}"
